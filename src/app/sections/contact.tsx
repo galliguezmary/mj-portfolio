dfbbd145d5cca3e42dae9744e40387e81fd19809
galliguezmary <galliguezmary@gmail.com>
2025-04-01 14:57:08 +0800

    Fixed unused useCallback error

--- src/app/sections/contact.tsx
+++ src/app/sections/contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useState, useCallback } from "react";
+import { useState } from "react";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
